d149ca6145ef144b64d5b2f6de3726c50205a207
memoDino <memodino00@gmail.com>
2024-10-23 02:54:12 -0600

    primer commit

---
+++ src/main/java/com/example/proyectoClups/ProyectoClupsApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoClups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoClups/ProyectoClupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoClups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoClupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
